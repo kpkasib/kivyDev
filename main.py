3d3363f8df1e1b70ff45507bd6065707bc00d27b
Kasib Bin Parvez <kasibparvez1996@gmail.com>
2023-09-23 09:44:03 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.button import Button
+from kivy.uix.stacklayout import StackLayout
+from kivy.metrics import dp
+
+
+
+
+
+class StackLayoutExample(StackLayout):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        self.orientation = 'lr-tb'
+
+        for i in range(0, 100):
+            
+            size = dp(100)
+            b = Button(text = str(i+1), size_hint= (None, None), size= (size, size))
+            self.add_widget(b)
+
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
+class BoxLayoutExample(BoxLayout):
+    pass
+
+"""    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.orientation = "vertical"
+        b1 = Button(text='A')
+        b2 = Button(text='B')
+        b3 = Button(text='C')
+        self.add_widget(b1)
+        self.add_widget(b2)
+        self.add_widget(b3)
+"""
+class MainWidget(Widget):
+    pass
+
+class TheLabApp(App):
+    pass
+
+
+TheLabApp().run()
